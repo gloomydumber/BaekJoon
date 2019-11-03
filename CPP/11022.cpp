957a6b3f0c737af63639ad944aad8275992a595e
gloomydumber <chromacrow@naver.com>
2019-11-03 17:00:34 +0900

    11022.cpp

---
+++ CPP/11022.cpp
@@ -0,0 +1,22 @@
+// https://www.acmicpc.net/problem/11022
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+  cin.tie(NULL);
+  ios_base::sync_with_stdio(false);
+
+  int T, A, B;
+  cin>> T;
+
+  for(int i=0; i<T; i++){
+    cin >> A>> B;
+    cout<< "Case #" <<i+1<<": "<<A<<' '<<"+ "<<B<<" = "<< A+B << '\n';
+  }
+
+  return 0;
+}
