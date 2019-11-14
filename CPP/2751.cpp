391d387a598514c5f6c601bbcf736c7230419759
gloomydumber <chromacrow@naver.com>
2019-11-14 15:49:54 +0900

    *2751.cpp

--- CPP/2751.cpp
+++ CPP/2751.cpp
@@ -1,6 +1,6 @@
 // https://www.acmicpc.net/problem/2751
 // https://www.acmicpc.net/board/view/31887
-
+// sort의 종류에 관해 정리 및 공부 할 것
 #include <iostream>
 #include <cstdio>
 #include <algorithm>
@@ -16,11 +16,10 @@ int main(void)
   for(int i=0; i<N; i++)
     scanf("%d",&arr[i]);
 
-  sort(arr,arr+N);
+  sort(arr,arr+N); // STL의 sort사용..
 
   for(int i=0; i<N; i++)
     printf("%d\n",arr[i]);
-
 
   delete []arr;
 
