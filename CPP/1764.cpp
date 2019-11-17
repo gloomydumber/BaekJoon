69155271463c0480b06af44f7d902ae32eed89fb
gloomydumber <chromacrow@naver.com>
2019-11-17 18:35:51 +0900

    !1764.cpp

--- CPP/1764.cpp
+++ CPP/1764.cpp
@@ -9,8 +9,8 @@ using namespace std;
 
 int main()
 {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
+	//ios_base::sync_with_stdio(0);
+	//cin.tie(0);
 
 	int n, m;
 	cin >> n >> m;
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # BaekJoon
 for BaekJoon problem solving
-
+*. : review
+!. : redo
 https://www.acmicpc.net/
