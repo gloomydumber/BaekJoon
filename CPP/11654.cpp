ecbf6d0e3ebcef3327a7b6b4917254f99f96cc7a
gloomydumber <chromacrow@naver.com>
2019-11-22 20:52:29 +0900

    11654.cpp

---
+++ CPP/11654.cpp
@@ -0,0 +1,15 @@
+// https://www.acmicpc.net/problem/11654
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+  char A;
+
+  cin>>A;
+  //cout<<(int)A; // 같은효과
+  cout<<A+0;
+  return 0;
+}
