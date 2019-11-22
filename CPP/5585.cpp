7c29d9d3af352359d20ac69ef1b1d508d39ca67c
gloomydumber <chromacrow@naver.com>
2019-11-22 20:34:26 +0900

    5585.cpp

--- CPP/5585.cpp
+++ CPP/5585.cpp
@@ -1,4 +1,5 @@
 // https://www.acmicpc.net/problem/5585
+// 지저분한데..
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -15,38 +16,28 @@ int main(void)
   int cnt=0;
 
   cin>>motte;
-  gohakyu = (1000-motte)/500;
 
-  cout<<gohakyu<<'\n';
+  gohakyu = (1000-motte)/500;
   motte=1000-motte-(500*gohakyu);
-  cout<<motte<<'\n';
 
   hakyu=motte/100;
   motte=motte-(100*hakyu);
-  cout<<hakyu<<endl;
-  cout<<motte<<'\n';
+
 
   gojyu=motte/50;
   motte=motte-(50*gojyu);
-  cout<<gojyu<<endl;
-  cout<<motte<<endl;
 
   jyu=motte/10;
   motte=motte-(10*jyu);
-  cout<<jyu<<endl;
-  cout<<motte<<endl;
 
   go=motte/5;
   motte=motte-(5*go);
-  cout<<go<<endl;
-  cout<<motte<<endl;
 
   ichi=motte/1;
   motte=motte-(1*ichi);
-  cout<<ichi<<endl;
-  cout<<motte<<endl;
 
   cnt=gohakyu+hakyu+gojyu+jyu+go+ichi;
   cout<<cnt<<endl;
 
+  return 0;
 }
