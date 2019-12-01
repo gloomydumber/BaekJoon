ad4a0a60b734b66123260fb117098d9128ce6ae4
gloomydumber <chromacrow@naver.com>
2019-12-01 21:01:44 +0900

    18108.cpp

---
+++ CPP/18108.cpp
@@ -0,0 +1,14 @@
+// https://www.acmicpc.net/problem/18108
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int telephone(char c);
+
+int main(void)
+{
+  int year;
+  cin>>year;
+  cout<<year-543<<'\n';
+  return 0;
+}
