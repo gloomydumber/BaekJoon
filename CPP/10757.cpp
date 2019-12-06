acaca285e712656f1b6fd7d5471ae77e652a08ee
gloomydumber <chromacrow@naver.com>
2019-12-06 21:57:08 +0900

    temp

---
+++ CPP/10757.cpp
@@ -0,0 +1,14 @@
+// https://www.acmicpc.net/problem/10757
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+  //long long A, B; //__int64 A,B;
+  //unsigned __int64 A, B;
+  unsigned long long A, B;
+  cin>>A>>B;
+  cout<<A+B;
+  return 0;
+}
