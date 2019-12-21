5796d304cc8c4f0509f6c632bcc72619f4578e1b
gloomydumber <chromacrow@naver.com>
2019-12-21 17:28:27 +0900

    !10757.cpp

--- CPP/10757.cpp
+++ CPP/10757.cpp
@@ -3,12 +3,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int summation (string A, string B)
+{
+  int tempA, tempB, temp, result;
+  string stemp1, stemp2;
+
+  while(!A.empty()||!B.empty())
+  {
+    stemp1=A.back();
+    tempA=stoi(stemp1);
+    stemp2=B.back();
+    tempB=stoi(stemp2);
+    result=tempA+tempB;
+    A.pop_back();
+    B.pop_back();
+    cout<<result<<'\n';
+  }
+
+
+
+  return temp;
+}
+
 int main(void)
 {
   //long long A, B; //__int64 A,B;
   //unsigned __int64 A, B;
-  unsigned long long A, B;
+  string A;
+  string B;
   cin>>A>>B;
+  summation(A, B);
   cout<<A+B;
   return 0;
 }
